b7ddbc1db9c4147daa54a7390c2bd856c7781666
Mohit Soni <mohitsoni9731@gmail.com>
2024-06-06 18:55:52 +0530

    Tried to add Proxies to handle CORS but is not working

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+     // server: {
+     //      proxy: {
+     //           '/': {
+     //                target: 'http://localhost:5000/',
+     //                changeOrigin:true,
+     //                secure:false
+     //           }
+     //      }
+     // },
+     plugins: [react()],
 })
