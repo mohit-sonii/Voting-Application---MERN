1e1d777c95526c9fc9e3868507499b43fb2b265d
Mohit Soni <mohitsoni9731@gmail.com>
2024-06-21 11:50:53 +0530

    update the axoios Instance for routing

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
      baseURL: process.env.NODE_ENV === 'production'
           ? 'https://voting-application-mern.vercel.app/'
-          : 'http://localhost:8000/api/v1',
+          : 'http://localhost:8000/',
 });
 
 export default api;
