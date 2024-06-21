a1dfd00addaece8a87946f0870f26456779e915d
Mohit Soni <mohitsoni9731@gmail.com>
2024-06-21 11:49:01 +0530

    Again updated the files to deploy on vercel properly

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
      baseURL: process.env.NODE_ENV === 'production'
-          ? 'https://voting-application-mern.vercel.app/api/v1'
+          ? 'https://voting-application-mern.vercel.app/'
           : 'http://localhost:8000/api/v1',
 });
 
