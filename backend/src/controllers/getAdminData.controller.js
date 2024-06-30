f77c0f5991107126a6a0a4901b2032fad3c324e8
Mohit Soni <mohitsoni9731@gmail.com>
2024-06-30 17:54:45 +0530

    trying #6 to deploy backend on vercel

--- backend/src/controllers/getAdminData.controller.js
+++ backend/src/controllers/getAdminData.controller.js
@@ -1,13 +1,12 @@
+import { Admin } from "../models/admin.model.js";
+import { asyncHandler } from "../utils/asyncHandler.util.js";
+import { apiResponse } from "../utils/response.util.js";
 
-import {Admin} from "../models/admin.model.js"
-import {asyncHandler} from "../utils/asyncHandler.util.js"
-import {apiResponse} from "../utils/response.util.js"
-
-export const getAdminDetails = asyncHandler(async(_,res)=>{
-     const adminData = await Admin.find()
-     return res.status(200).json(new apiResponse(
-          200,
-          "Admin Data fetched successfully",
-          adminData
-     ))
-})
+export const getAdminDetails = asyncHandler(async (_, res) => {
+    const adminData = await Admin.find();
+    return res.status(200).json(new apiResponse(
+        200,
+        "Admin Data fetched successfully",
+        adminData
+    ));
+});
--- backend/src/utils/Response.util.js
+++ backend/src/utils/Response.util.js
@@ -1,12 +1,13 @@
-
+// response.util.js
 
 class apiResponse {
-     constructor(statusCode, message = "success", data){
-          this.statusCode = statusCode
-          this.message = message
-          this.data = data,
-               this.success = statusCode < 400
+     constructor(statusCode, message = "success", data) {
+         this.statusCode = statusCode;
+         this.message = message;
+         this.data = data;
+         this.success = statusCode < 400;
      }
-}
-
-export {apiResponse}
+ }
+ 
+ export { apiResponse };
+ 
