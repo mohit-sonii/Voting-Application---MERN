63143f67097b921aa5afc79a8a7b4c8e6dd0580c
Mohit Soni <mohitsoni9731@gmail.com>
2024-07-05 12:59:28 +0530

    corrected Register component to save file as well

--- frontend/src/Components/ProfileUpdate.jsx
+++ frontend/src/Components/ProfileUpdate.jsx
@@ -9,179 +9,178 @@ import { server, serverWithId } from "../server"
 
 function ProfileUpdate() {
 
-     const { userData } = useContext(userContext)
-     const [data, setData] = useState({})
-     const navigate = useNavigate()
-     const [avatarPreview, setAvatarPreview] = useState(userData.avatar)
-     const { visitorType } = useContext(userContext)
-     const userId = useSelector(state => state.userValues.userId)
-     const [err, setErr] = useState('')
-     const [success, setSuccess] = useState('')
+  const { userData } = useContext(userContext)
+  const [data, setData] = useState({})
+  const navigate = useNavigate()
+  const [avatarPreview, setAvatarPreview] = useState(userData.avatar)
+  const { visitorType } = useContext(userContext)
+  const userId = useSelector(state => state.userValues.userId)
+  const [err, setErr] = useState('')
+  const [success, setSuccess] = useState('')
 
-     useEffect(() => {
-          setData({
-               firstName: userData.firstName,
-               lastName: userData.lastName
-          })
-     }, [userData])
+  useEffect(() => {
+    setData({
+      firstName: userData.firstName,
+      lastName: userData.lastName
+    })
+  }, [userData])
 
-     const handleChange = (e) => {
-          const { name, value, files } = e.target
-          if (name === 'avatar') {
-               const file = files[0]
-               console.log(file.path)
-               setData({ ...data, [name]: file })
-               setAvatarPreview(URL.createObjectURL(file))
+  const handleChange = (e) => {
+    const { name, value, files } = e.target
+    if (name === 'avatar') {
+      const file = files[0]
+      setData({ ...data, [name]: file })
+      setAvatarPreview(URL.createObjectURL(file))
 
-          } else {
-               setData({ ...data, [name]: value })
-          }
-     }
+    } else {
+      setData({ ...data, [name]: value })
+    }
+  }
 
-     const handleSubmit = async (e) => {
-          e.preventDefault()
-          try {
-               const token = localStorage.getItem('accessToken')
-               await api.patch(`${serverWithId}/${userId}/api/v1/user/profile/update`, data, {
-                    headers: {
-                         Authorization: `Bearer ${token}`,
-                         "Content-Type": "multipart/form-data"
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const token = localStorage.getItem('accessToken')
+      await api.patch(`${serverWithId}/${userId}/api/v1/user/profile/update`, data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "multipart/form-data"
 
-                    }
-               })
-               setSuccess('Credentials Updated Successfully')
-               navigate(`/${userId}/api/v1/user/profile/update`)
+        }
+      })
+      setSuccess('Credentials Updated Successfully')
+      navigate(`/${userId}/api/v1/user/profile/update`)
 
-          } catch (error) {
-               setErr(error.response?.data?.message || 'Sorry for inconvenience, I am working to resolve this issue')
-          }
-     }
+    } catch (error) {
+      setErr(error.response?.data?.message || 'Sorry for inconvenience, I am working to resolve this issue')
+    }
+  }
 
-     useEffect(() => {
-          const timer = setTimeout(() => {
-               setErr('');
-               setSuccess('')
-          }, 2000);
-          return () => clearTimeout(timer);
-     }, [err, success]);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setErr('');
+      setSuccess('')
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, [err, success]);
 
-     return (
-          <>
-               {err &&
-                    <div className="errorField">
-                         <p>{err}</p>
-                    </div>
-               }
+  return (
+    <>
+      {err &&
+        <div className="errorField">
+          <p>{err}</p>
+        </div>
+      }
 
-               <div className="formSection mt-10">
-                    <div className="form-headings">
-                         <p className=' text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-extrabold text-left'>Update Profile</p>
-                    </div>
+      <div className="formSection mt-10">
+        <div className="form-headings">
+          <p className=' text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-extrabold text-left'>Update Profile</p>
+        </div>
 
-                    <form className="flex" onSubmit={handleSubmit} >
-                         <div className="allEnteries w-full sm:w-[50%] flex flex-col gap-10  justify-between">
-                              <div className="header flex gap-10">
-                                   {userData.avatar ?
-                                        <img src={avatarPreview} alt="User Avatar" className="avatar object-cover " />
-                                        : <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkVto1t4ROcxcqi0SrFtgSQQJ1Aau0ad1d3g&s" alt="No Avatar" className="avatar" />
-                                   }
-                                   <Button innerText="Update Avatar" onClick={() => document.getElementById('fileInput').click()} />
-                                   <input
-                                        type="file"
-                                        name='avatar'
-                                        id="fileInput"
-                                        style={{ display: 'none' }}
-                                        onChange={handleChange}
-                                   />
-                              </div>
-                              <div className="formData flex flex-col gap-10">
-                                   <div className="ele firstName flex flex-col gap-3">
-                                        <label htmlFor="firstName" className="text-lg lg:text-xl xl:text-2xl">First Name</label>
-                                        <input
-                                             className="text-lg lg:text-xl xl:text-2xl"
-                                             type="text"
-                                             name="firstName"
-                                             id="firstName"
-                                             onChange={handleChange}
-                                             value={data.firstName || ''}
-                                        />
-                                   </div>
-                                   <div className="ele lastname flex flex-col gap-3">
-                                        <label htmlFor="lastName" className="text-lg lg:text-xl xl:text-2xl">Last Name</label>
-                                        <input
-                                             className="text-lg lg:text-xl xl:text-2xl"
-                                             type="text"
-                                             name="lastName"
-                                             id="lastName"
-                                             onChange={handleChange}
-                                             value={data.lastName || ''}
-                                        />
-                                   </div>
-                                   <div className="ele uniqueId flex flex-col gap-3">
-                                        <label htmlFor="uniqueId" className="text-lg lg:text-xl xl:text-2xl">Aadhar card Number</label>
-                                        <input
-                                             className="text-lg lg:text-xl xl:text-2xl"
-                                             type="number"
-                                             name="uniqueId"
-                                             id="uniqueId"
-                                             defaultValue={userData.uniqueId}
-                                             disabled
-                                        />
-                                   </div>
-                                   <div className="ele voterId flex flex-col gap-3">
-                                        <label htmlFor="voterId" className="text-lg lg:text-xl xl:text-2xl">Voter Card Id</label>
-                                        <input
-                                             className="text-lg lg:text-xl xl:text-2xl"
-                                             type="number"
-                                             name="voterId"
-                                             id="voterId"
-                                             defaultValue={userData.voterId}
-                                             disabled
-                                        />
-                                   </div>
-                                   <div className="ele isVoted flex flex-col gap-3">
-                                        <label htmlFor="isVoted" className="text-lg lg:text-xl xl:text-2xl">Voted :</label>
-                                        <input
-                                             className="text-lg lg:text-xl xl:text-2xl"
-                                             type="text"
-                                             name="isVoted"
-                                             id="isVoted"
-                                             defaultValue={userData.isVoted}
-                                             disabled
-                                        />
-                                   </div>
+        <form className="flex" onSubmit={handleSubmit} >
+          <div className="allEnteries w-full sm:w-[50%] flex flex-col gap-10  justify-between">
+            <div className="header flex gap-10">
+              {userData.avatar ?
+                <img src={avatarPreview} alt="User Avatar" className="avatar object-cover " />
+                : <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkVto1t4ROcxcqi0SrFtgSQQJ1Aau0ad1d3g&s" alt="No Avatar" className="avatar" />
+              }
+              <Button innerText="Update Avatar" onClick={() => document.getElementById('fileInput').click()} />
+              <input
+                type="file"
+                name='avatar'
+                id="fileInput"
+                style={{ display: 'none' }}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="formData flex flex-col gap-10">
+              <div className="ele firstName flex flex-col gap-3">
+                <label htmlFor="firstName" className="text-lg lg:text-xl xl:text-2xl">First Name</label>
+                <input
+                  className="text-lg lg:text-xl xl:text-2xl"
+                  type="text"
+                  name="firstName"
+                  id="firstName"
+                  onChange={handleChange}
+                  value={data.firstName || ''}
+                />
+              </div>
+              <div className="ele lastname flex flex-col gap-3">
+                <label htmlFor="lastName" className="text-lg lg:text-xl xl:text-2xl">Last Name</label>
+                <input
+                  className="text-lg lg:text-xl xl:text-2xl"
+                  type="text"
+                  name="lastName"
+                  id="lastName"
+                  onChange={handleChange}
+                  value={data.lastName || ''}
+                />
+              </div>
+              <div className="ele uniqueId flex flex-col gap-3">
+                <label htmlFor="uniqueId" className="text-lg lg:text-xl xl:text-2xl">Aadhar card Number</label>
+                <input
+                  className="text-lg lg:text-xl xl:text-2xl"
+                  type="number"
+                  name="uniqueId"
+                  id="uniqueId"
+                  defaultValue={userData.uniqueId}
+                  disabled
+                />
+              </div>
+              <div className="ele voterId flex flex-col gap-3">
+                <label htmlFor="voterId" className="text-lg lg:text-xl xl:text-2xl">Voter Card Id</label>
+                <input
+                  className="text-lg lg:text-xl xl:text-2xl"
+                  type="number"
+                  name="voterId"
+                  id="voterId"
+                  defaultValue={userData.voterId}
+                  disabled
+                />
+              </div>
+              <div className="ele isVoted flex flex-col gap-3">
+                <label htmlFor="isVoted" className="text-lg lg:text-xl xl:text-2xl">Voted :</label>
+                <input
+                  className="text-lg lg:text-xl xl:text-2xl"
+                  type="text"
+                  name="isVoted"
+                  id="isVoted"
+                  defaultValue={userData.isVoted}
+                  disabled
+                />
+              </div>
 
-                                   <div className="button flex gap-6">
-                                        <button type="button" id="back" className="text-2xl 2xl:text-3xl py-4 2xl:py-10 px-10 2xl:px-24">
-                                             <Link to={`/${userId}`} state={visitorType}>
-                                                  <span>&lt; Back</span>
-                                             </Link>
-                                        </button>
-                                        <button
-                                             type="submit"
-                                             id="button"
-                                             className={`text-2xl 2xl:text-3xl py-4 2xl:py-10 px-10 2xl:px-24 ${Object.keys(data).length === 0 ? 'disabled' : ''}`}
-                                             disabled={Object.keys(data).length === 0}
-                                        >
-                                             <span>Submit</span>
-                                        </button>
-                                        <button type="button" id="button" className="text-2xl 2xl:text-3xl py-4 2xl:py-10 px-10 2xl:px-24">
-                                             <Link to={'password'} state={visitorType}>
-                                                  <span>Update Password</span>
-                                             </Link>
-                                        </button>
-                                        {success &&
-                                             <div className="errorField greenField">
-                                                  <p>{success}</p>
-                                             </div>
-                                        }
-                                   </div>
-                              </div>
-                         </div>
-                    </form>
-               </div>
-          </>
-     )
+              <div className="button flex gap-6">
+                <button type="button" id="back" className="text-2xl 2xl:text-3xl py-4 2xl:py-10 px-10 2xl:px-24">
+                  <Link to={`/${userId}`} state={visitorType}>
+                    <span>&lt; Back</span>
+                  </Link>
+                </button>
+                <button
+                  type="submit"
+                  id="button"
+                  className={`text-2xl 2xl:text-3xl py-4 2xl:py-10 px-10 2xl:px-24 ${Object.keys(data).length === 0 ? 'disabled' : ''}`}
+                  disabled={Object.keys(data).length === 0}
+                >
+                  <span>Submit</span>
+                </button>
+                <button type="button" id="button" className="text-2xl 2xl:text-3xl py-4 2xl:py-10 px-10 2xl:px-24">
+                  <Link to={'password'} state={visitorType}>
+                    <span>Update Password</span>
+                  </Link>
+                </button>
+                {success &&
+                  <div className="errorField greenField">
+                    <p>{success}</p>
+                  </div>
+                }
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  )
 }
 
 export default ProfileUpdate
--- frontend/src/Components/Register.jsx
+++ frontend/src/Components/Register.jsx
@@ -5,132 +5,133 @@ import React from 'react'
 import { server } from "../server.js"
 import { Link } from "react-router-dom"
 import { useNavigate } from "react-router-dom"
+import axios from "axios"
 import { useDispatch } from "react-redux"
 import { setUserId } from "../Redux/slicer.js"
 import { userContext } from "../context.js"
 
 function Register() {
-  const dispatch = useDispatch()
-  const [data, setData] = useState({
-    firstName: '',
-    lastName: '',
-    voterId: '',
-    uniqueId: '',
-    password: '',
-    avatar: ""
-  })
-  const [err, setErr] = useState('')
-  const navigate = useNavigate()
-  const { changeVisitorType } = useContext(userContext)
-  const { updateVisitorId } = useContext(userContext)
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    if (name === "avatar") {
-      setData({ ...data, avatar: e.target.files[0] });
-    } else {
-      setData({ ...data, [name]: value })
-    }
-  }
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post(`${server}/auth/register`, data, {
-        headers: {
-          "Content-Type": "multipart/form-data"
-        }
-      })
-      console.log(response.data)
-      if (response.status === 200) {
-        setData({
-          firstName: '',
-          voterId: '',
-          uniqueId: '',
-          lastName: '',
-          password: '',
-          avatar: ''
-        })
-        changeVisitorType('user')
-        localStorage.setItem('accessToken', response.data.data.accessToken)
-        updateVisitorId(response.data.data.user._id)
-        dispatch(setUserId(response.data.data.user._id))
-        navigate(`/${response.data.data.user._id}`)
+   const dispatch = useDispatch()
+   const [data, setData] = useState({
+      firstName: '',
+      lastName: '',
+      voterId: '',
+      uniqueId: '',
+      password: '',
+      avatar: ""
+   })
+   const [err, setErr] = useState('')
+   const navigate = useNavigate()
+   const { changeVisitorType } = useContext(userContext)
+   const { updateVisitorId } = useContext(userContext)
+   const handleChange = (e) => {
+      const { name, value } = e.target;
+      if (name === "avatar") {
+         setData({ ...data, avatar: e.target.files[0] });
+      } else {
+         setData({ ...data, [name]: value })
       }
-      else {
-        throw new Error("Error while register a user")
+   }
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+         const response = await axios.post(`${server}/auth/register`, data, {
+            headers: {
+               "Content-Type": "multipart/form-data"
+            }
+         })
+         if (response.status === 200) {
+            setData({
+               firstName: '',
+               voterId: '',
+               uniqueId: '',
+               lastName: '',
+               password: '',
+               avatar: ''
+            })
+            changeVisitorType('user')
+            localStorage.setItem('accessToken', response.data.data.accessToken)
+            updateVisitorId(response.data.data.user._id)
+            dispatch(setUserId(response.data.data.user._id))
+            navigate(`/${response.data.data.user._id}`)
+         }
+         else {
+            throw new Error("Error while register a user")
+         }
+      } catch (err) {
+         console.log(err.message, err)
+         setErr(err.response?.data?.message || 'Sorry for inconvenience, I am trying to resolve the issue')
       }
-    } catch (err) {
-      setErr(err.response?.data?.message || 'Sorry for inconvenience, I am trying to resolve the issue')
-    }
-  }
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setErr('');
-    }, 2000);
+   }
+   useEffect(() => {
+      const timer = setTimeout(() => {
+         setErr('');
+      }, 2000);
 
-    return () => clearTimeout(timer);
-  }, [err]);
-  return (
-    <div id="signup">
-      {err &&
-        <div className="errorField">
-          <p>{err}</p>
-        </div>
-      }
-      <div className="signup">
-        <div className="signup-heading flex gap-5 flex-col">
-          <p className='text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-extrabold'>Register</p>
-          <p className=" flex flex-row gap-5 sm:text-2xl lg:text-3xl xl:text-2xl font-semibold">Already Have an Account?
-            <Link to="/api/v1/auth/login">
-              <span className="login">Login</span>
-            </Link>
-          </p>
-        </div>
-        <form className="flex" onSubmit={handleSubmit}>
-          <div className="signup-form grid gap-10 sm:grid-cols-2">
-            <div className="field firstName">
-              <label className="text-2xl 2xl:text-4xl font-light" htmlFor="firstName">Enter your First Name</label>
-              <input className="sm:w-3/4 text-sm lg:text-lg xl:text-xl 2xl:text-2xl  font-semibold" name="firstName" id="firstName" type="text" onChange={handleChange} value={data.firstName} required />
+      return () => clearTimeout(timer);
+   }, [err]);
+   return (
+      <div id="signup">
+         {err &&
+            <div className="errorField">
+               <p>{err}</p>
             </div>
-
-            <div className="field lastName">
-              <label className="text-lg lg:text-xl xl:text-2xl 2xl:text-3xl font-light" htmlFor="lastName">Enter your Last Name</label>
-              <input className="sm:w-3/4 text-sm lg:text-lg xl:text-xl 2xl:text-2xl  font-semibold" name="lastName" id="lastName" required type="text" onChange={handleChange} value={data.lastName} />
+         }
+         <div className="signup">
+            <div className="signup-heading flex gap-5 flex-col">
+               <p className='text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-extrabold'>Register</p>
+               <p className=" flex flex-row gap-5 sm:text-2xl lg:text-3xl xl:text-2xl font-semibold">Already Have an Account?
+                  <Link to="/api/v1/auth/login">
+                     <span className="login">Login</span>
+                  </Link>
+               </p>
             </div>
+            <form className="flex" onSubmit={handleSubmit}>
+               <div className="signup-form grid gap-10 sm:grid-cols-2">
+                  <div className="field firstName">
+                     <label className="text-2xl 2xl:text-4xl font-light" htmlFor="firstName">Enter your First Name</label>
+                     <input className="sm:w-3/4 text-sm lg:text-lg xl:text-xl 2xl:text-2xl  font-semibold" name="firstName" id="firstName" type="text" onChange={handleChange} value={data.firstName} required />
+                  </div>
 
-            <div className="field uniqueId">
-              <label className="text-lg lg:text-xl xl:text-2xl 2xl:text-3xl font-light" htmlFor="uniqueId">Enter Aadhar Card Number</label>
-              <input className="sm:w-3/4 text-sm lg:text-lg xl:text-xl 2xl:text-2xl  font-semibold" name="uniqueId" id="uniqueId" type="text" required maxLength={'12'} onChange={handleChange} value={data.uniqueId} />
-            </div>
+                  <div className="field lastName">
+                     <label className="text-lg lg:text-xl xl:text-2xl 2xl:text-3xl font-light" htmlFor="lastName">Enter your Last Name</label>
+                     <input className="sm:w-3/4 text-sm lg:text-lg xl:text-xl 2xl:text-2xl  font-semibold" name="lastName" id="lastName" required type="text" onChange={handleChange} value={data.lastName} />
+                  </div>
 
-            <div className="field voterId">
-              <label className="text-lg lg:text-xl xl:text-2xl 2xl:text-3xl font-light" htmlFor="voterId">Enter your Voter Card Number</label>
-              <input className="sm:w-3/4 text-sm lg:text-lg xl:text-xl 2xl:text-2xl  font-semibold" name="voterId" id="voterId" type="text" maxLength={7} required onChange={handleChange} value={data.voterId} />
-            </div>
+                  <div className="field uniqueId">
+                     <label className="text-lg lg:text-xl xl:text-2xl 2xl:text-3xl font-light" htmlFor="uniqueId">Enter Aadhar Card Number</label>
+                     <input className="sm:w-3/4 text-sm lg:text-lg xl:text-xl 2xl:text-2xl  font-semibold" name="uniqueId" id="uniqueId" type="text" required maxLength={'12'} onChange={handleChange} value={data.uniqueId} />
+                  </div>
+
+                  <div className="field voterId">
+                     <label className="text-lg lg:text-xl xl:text-2xl 2xl:text-3xl font-light" htmlFor="voterId">Enter your Voter Card Number</label>
+                     <input className="sm:w-3/4 text-sm lg:text-lg xl:text-xl 2xl:text-2xl  font-semibold" name="voterId" id="voterId" type="text" maxLength={7} required onChange={handleChange} value={data.voterId} />
+                  </div>
 
 
-            <div className="field password">
-              <label className="text-lg lg:text-xl xl:text-2xl 2xl:text-3xl font-light" htmlFor="password">Create Password </label>
-              <input className="sm:w-3/4 text-sm lg:text-lg xl:text-xl 2xl:text-2xl  font-semibold" name="password" id="password" required type="password" onChange={handleChange} value={data.password} />
-            </div>
-            <div className="field ">
-              <label className="text-lg lg:text-xl xl:text-2xl 2xl:text-3xl font-light" htmlFor="avatar">Upload your Avatar </label>
-              <input className="sm:w-3/4 text-sm lg:text-lg xl:text-xl 2xl:text-2xl  font-semibold" id="avatar" name="avatar" type="file" onChange={handleChange} />
-            </div>
-          </div>
-          <div className="button flex gap-8">
-            <button type="submit" id="button" className="text-2xl 2xl:text-3xl py-4 2xl:py-10 px-10 2xl:px-24"><span>Register</span></button>
+                  <div className="field password">
+                     <label className="text-lg lg:text-xl xl:text-2xl 2xl:text-3xl font-light" htmlFor="password">Create Password </label>
+                     <input className="sm:w-3/4 text-sm lg:text-lg xl:text-xl 2xl:text-2xl  font-semibold" name="password" id="password" required type="password" onChange={handleChange} value={data.password} />
+                  </div>
+                  <div className="field ">
+                     <label className="text-lg lg:text-xl xl:text-2xl 2xl:text-3xl font-light" htmlFor="avatar">Upload your Avatar </label>
+                     <input className="sm:w-3/4 text-sm lg:text-lg xl:text-xl 2xl:text-2xl  font-semibold" id="avatar" name="avatar" type="file" onChange={handleChange} />
+                  </div>
+               </div>
+               <div className="button flex gap-8">
+                  <button type="submit" id="button" className="text-2xl 2xl:text-3xl py-4 2xl:py-10 px-10 2xl:px-24"><span>Register</span></button>
 
-            <button type="button" id="back" className="text-2xl 2xl:text-3xl py-4 2xl:py-10 px-10 2xl:px-24">
-              <Link to="/">
-                <span>&lt; Back</span>
-              </Link>
-            </button>
-          </div>
-        </form>
-      </div>
-    </div >
+                  <button type="button" id="back" className="text-2xl 2xl:text-3xl py-4 2xl:py-10 px-10 2xl:px-24">
+                     <Link to="/">
+                        <span>&lt; Back</span>
+                     </Link>
+                  </button>
+               </div>
+            </form>
+         </div>
+      </div >
 
-  )
+   )
 }
 
 export default Register
